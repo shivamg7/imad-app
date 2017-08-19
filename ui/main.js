43acd94f5bdddf546452eca55d5b088e6d616959
shivamg7 <playfullittlekid@gmail.com>
2017-08-19 13:48:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,14 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img=document.getElementById("madi");
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick = function() {
-    img.style.marginLeft = "300px";
+    var interval = setInterval(moveRight, 100);
+    
+    
 }
