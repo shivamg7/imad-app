05b2facb04ecd36c2859a214d78bb522be7cffb4
shivamg7 <playfullittlekid@gmail.com>
2017-08-19 12:07:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,90 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one' : {
+    title: 'Artticle one! Shivam Gupta',
+    heading: 'Article one',
+    date : 'Sep 5, 2017',
+    content : `     <p>
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                    This is the start of my first article and I have ton of assigments lined up from               college.
+                    </p>`
+                    },
+    'article-two' : {
+    title: 'Artticle Two! Shivam Gupta',
+    heading: 'Article Two',
+        date : 'Sep 10, 2017',
+        content : `     <p>
+                        This is the content for my third article.
+                        </p>`
+    },
+    
+    'article-three' : {
+    title: 'Artticle Three! Shivam Gupta',
+    heading: 'Article Three',
+        date : 'Sep 15, 2017',
+        content : `     <p>
+        This is the content for my third article.
+            </p>`
+            }
+
+    
+};
+
+
+function createTemplate(data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate  = `
+<html>
+        <head>
+        <title>
+${title}
+        </title>
+            <meta name="viewport" content="width=device-width,intial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+
+    <body>
+        <div class="container">
+            <div>
+            <a =href="/"/>HOME</a>
+            </div>
+            <hr/>
+            <h3>
+${heading}
+            </h3>
+            <div>
+${date}
+            </div>
+            <div>
+${content}
+            </div>
+        </div
+    </body>
+</html>
+`;
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/:articleName',function (req, res) {
+        //articleName == article-one
+        //articles[articleName]== {} content objects for article one
+        var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+        });
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -21,7 +102,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
