bf7e93b6b7eb5dfc4724c86700b6e1d29b7da72a
shivamg7 <playfullittlekid@gmail.com>
2017-08-19 13:42:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-alert("Load content");
+
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 
@@ -8,6 +8,6 @@ element.innerHTML = 'New Value';
 
 //move the image
 var img=document.getElementbyId("madi");
-img.onCLick = function() {
+img.onclick = function() {
     img.style.marginLeft = "100px";
 }
