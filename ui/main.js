d07f47b91324a00b742f9ba88fb2b9081de1af21
shivamg7 <playfullittlekid@gmail.com>
2017-08-23 22:40:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 
 var button=document.getElementById("counter");
 
-
+/*
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 //Move the image
@@ -15,7 +15,7 @@ function moveRight() {
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
 }
-
+*/
 
 button.onclick = function() {
     //Create a request object
@@ -47,17 +47,34 @@ var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
     //Make a request to the server and send the name
+     var request =  new XMLHttpRequest();
+    //Capture the response and store it in a variable
     
+    
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            //Take some action
+            if(request.status==200)
+            {
+                    var names = ['name1','name2','name3'];
+                    var list = '';
+                    for(var i=0 ; i < names.length; i++)
+                    {
+                        
+                        list += '<li>' + names[i] + '</li>';
+                    } 
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML = list ;
+            }
+        }
+        //Not done yet
+    };
+    //Make the request
+        request.open('GET','http://shivamxav.imad.hasura-app.io/submit-name?name='+name,true);
+        request.send(null);
     //Capture a list of names and render it as a list
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0 ; i < names.length; i++)
-    {
-        
-        list += '<li>' + names[i] + '</li>';
-    } 
-    var ul=document.getElementById('namelist');
-    ul.innerHTML = list ;
+   
     
     
 }
