1daeb861983225130956dce64001af638b38e4e1
shivamg7 <playfullittlekid@gmail.com>
2017-08-23 21:15:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,3 +40,22 @@ button.onclick = function() {
         request.open('GET','http://shivamxav.imad.hasura-app.io/counter',true);
         request.send(null);
 };
+
+//submit name
+var nameInput=document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
+    //Make a request to the server and send the name
+    
+    //Capture a list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0 ; i < names.length; i++) {
+        list+ = '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById("namelist");
+    ul.innetHTML = list ;
+    
+    
+}
