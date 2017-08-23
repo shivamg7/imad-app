aacd3e70a14c3aff76fdadb3e49047dc1e3befd1
shivamg7 <playfullittlekid@gmail.com>
2017-08-23 21:21:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,9 +50,11 @@ submit.onclick = function() {
     
     //Capture a list of names and render it as a list
     var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0 ; i < names.length; i++) {
-        list+ = '<li>' + names[i] + '</li>';
+    var list = "";
+    for(var i=0 ; i < names.length; i++)
+    {
+        
+        list += '<li>' + names[i] + '</li>';
     }
     var ul=document.getElementById("namelist");
     ul.innetHTML = list ;
