88dd228a15a1ee9dee43e4b3a2eb217ce64e32c2
shivamg7 <playfullittlekid@gmail.com>
2017-08-23 22:42:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,7 +57,8 @@ submit.onclick = function() {
             //Take some action
             if(request.status==200)
             {
-                    var names = ['name1','name2','name3'];
+                    var names = request.responseText;
+                    names=JSON.parse(names);
                     var list = '';
                     for(var i=0 ; i < names.length; i++)
                     {
