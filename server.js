4c57d1a7bdb61a6a12317f7578f05af5fcffd53f
shivamg7 <playfullittlekid@gmail.com>
2017-08-26 16:16:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,12 +122,12 @@ app.get("/submit-name", function( req, res){ //changing :name to ?. now URL /sub
 
 });
 
-app.get('/article/:articleName',function (req, res) {
+app.get('/articles/:articleName',function (req, res) {
         //articleName == article-one
         //articles[articleName]== {} content objects for article one
         //var articleName = req.params.articleName;
         
-        pool.query("SELECT * FROM article WHERE title = " + res.params.articleName, function(err, result) {
+        pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err, result) {
             if(err) {
                 alert('Error generated');
                 res.status(500).send(err, toString());
